6fdfc10a3b8c5e6d4b645a98bbf14b4836d4b86f
nkg-laufire <nkg@laufire.in>
2022-10-11 11:24:30 +0530

    Example, how react re-renders its components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Parent = ({ children }) =>
+  {
+    const [count, setCount] = useState(0);
+
+    return (
+      <div onClick={() => setCount((count) => count + 1)}>
+        count: { count }
+        <div>{ children }</div>
+      </div>
+    );
+  };
+
+const Children = () => <span>{ Date.now() }</span>;
+
+const App = () => 
+  <div>
+    <Parent>
+      <Children></Children>
+    </Parent>
+  </div>;
 
 export default App;
