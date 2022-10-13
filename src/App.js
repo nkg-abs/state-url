6fe8e153c41491bf707941ddb482a6915cfcd25e
nkg-laufire <nkg@laufire.in>
2022-10-13 16:01:25 +0530

    Updated URL withoutre-renders.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import jsonUrl from 'json-url';
+import React from 'react'
 
-const App = () => {
-  const [student, setStudent] = useState({});
-  const { compress } = jsonUrl('lzma');
-  const { clipboard } = navigator;
-
-  useEffect(() => {
-    (async () => {
-      const stateUrl = new URLSearchParams(window.location.search).get('state');
-      const { decompress } = jsonUrl('lzma');
-
-      setStudent(await decompress(stateUrl));
-    })();
-  }, []);
-  
-  return (
-    <div>
-      <button onClick={async () => {
-        const encodedState = await compress(student);
-
-        clipboard.writeText(`https/localhost:3003?state=${ encodedState }`);
-      }}>
-        Get Link!
-      </button>
-    </div>
-  );
-};
+const App = () =>
+  <div>
+    <button onClick={() =>
+      window.history.pushState({}, '', '/clicked')}>
+      Change URL
+    </button>
+  </div>
 
 export default App;
