7a26608c666d2b4dda61432a655ed375e13bc957
nkg-abs <nkg@abs.in>
2023-03-01 13:53:35 +0530

    Implemented fetch and create action on todos.

--- src/App.js
+++ src/App.js
@@ -1,110 +1,58 @@
-import React, { useEffect, useState } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-  useNavigate
-} from "react-router-dom";
-import jsonUrl from 'json-url';
+import React from "react";
+import { useEffect, useState } from "react";
+import ApplicationStore from "./ApplicationStore";
+import RemoteStore from "./RemoteStore";
+import { peek } from '@laufire/utils/debug';
 
-export default function App() {
-  return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/post/hello-world">Post</Link>
-            </li>
-            <li>
-              <Link to="/users/?state=XQAAAAIBAAAAAAAAAAACwfv____gAAAA">Users</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/post/:slug" element={<Post />} />
-          <Route path="/users/" element={<Users />} /> 
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const App = () => {
+  const [state, setState] = useState({
+    todos: [{ id: 1 }],
+  });
 
-// Old way to fetch history
-function Post(props) {
-  console.log('Post');
-  return (
-    <div>
-      In React Router v4, you get the history object from props. <br />
-      <button type="button" onClick={() => props.history.goBack()}>
-        Go back
-      </button>
-    </div>
-  );
-}
-
-// new way to fetch history with hooks
-function Users() {
-  const navigate = useNavigate();
-  const { compress } = jsonUrl('lzma');
-  const { clipboard } = navigator;
-  const stateUrl = new URLSearchParams(window.location.search).get('state');
-  const [counter, setCounter] = useState(0);
-  
   useEffect(() => {
+    const appStore = ApplicationStore({
+      data: {
+        setState,
+      }
+    });
+
+    const remoteStore = RemoteStore({
+			pipe: (pipeContext) => { appStore(pipeContext); },
+			data: {
+				url: 'http://localhost:6005/',
+			},
+		});
+
     (async () => {
-      const { compress } = jsonUrl('lzma');
-      const stateUrl = new URLSearchParams(window.location.search).get('state');
-      const newState = await compress(counter);
-      stateUrl ===  newState || navigate(`/users/?state=${ newState }`);
-    })();
-  }, [counter, navigate]);
-  useEffect(() => {
-    (async () => {  
-      const { decompress } = jsonUrl('lzma');
-      const state = await decompress(stateUrl);
-      setCounter(state);
-      console.log(state, 'State updated');
-    })();
-  }, [stateUrl]);
-  
-  return (
-    <div>
-      In React Router v5, You can use hooks to get history object.
-      <br />
-      <button type="button" onClick={() => navigate(-1)}>
-        Go back
-      </button>
-      <button onClick={() => {
-        setCounter(count => {
-          const updatedCount = count + 1;
+      await remoteStore({
+        entity: 'todos',
+        action: 'read',
+      });
 
-          return updatedCount;
-        });
-      }}>
-        change Counter { counter }
-      </button>
-      <button onClick={async () => {
-        const encodedState = await compress(counter);
+      // NOTE: try to update through appStore.
+      setState((state) => ({
+        ...state,
+        appStore,
+        remoteStore,
+      }))
+    })(); 
+  }, []);
 
-        clipboard.writeText(`http://localhost:3003/users/?state=${ encodedState }`);
-      }}>
-        Get Link!
-      </button>
-    </div>
-  );
-}
+  peek(state);
 
-function Home() {
-  return (
-    <div className="App">
-      <h1>React Router useHistory hook</h1>
-      <h2>Inspect the code and click on the menu above</h2>
-    </div>
-  );
-}
+  return (<div onClick={async () => {
+    await state.remoteStore({
+			action: 'create',
+			entity: 'todos',
+			data: {
+				text: 'hello',
+				completed: false,
+			},
+		})
+    console.log(state);
+  }}>
+    appp
+  </div>);
+};
+
+export default App;
